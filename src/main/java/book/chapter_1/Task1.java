0727f185c52a12a7061c79bc833caf713e76219c
Natalia Orlyk <oksenyuk.n@gmail.com>
2015-11-18 17:32:45 +0200

    Initial commit

---
+++ src/main/java/book/chapter_1/Task1.java
@@ -0,0 +1,10 @@
+package book.chapter_1;
+
+/**
+ * Created by home on 18.11.2015.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
