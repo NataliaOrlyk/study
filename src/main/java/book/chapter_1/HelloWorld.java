ef2a4fb01ab788b429d73a6ca5b7124fc4abe681
Natalia Orlyk <oksenyuk.n@gmail.com>
2015-11-18 18:42:17 +0200

    First commit from IDEA

---
+++ src/main/java/book/chapter_1/HelloWorld.java
@@ -0,0 +1,10 @@
+package book.chapter_1;
+
+/**
+ * Created by home on 18.11.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
