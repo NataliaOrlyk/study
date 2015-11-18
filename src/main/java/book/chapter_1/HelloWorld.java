4ddf24889c883776f0a4023cc68a617524564386
Natalia Orlyk <oksenyuk.n@gmail.com>
2015-11-18 19:09:50 +0200

    Method example

--- src/main/java/book/chapter_1/HelloWorld.java
+++ src/main/java/book/chapter_1/HelloWorld.java
@@ -5,6 +5,12 @@ package book.chapter_1;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(add5ToNumber(10));
+        System.out.println(add5ToNumber(15));
+        System.out.println(add5ToNumber(20));
+    }
+
+    public static int add5ToNumber(int arg) {
+        return  arg + 5;
     }
 }
