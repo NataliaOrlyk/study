9047da8f6b447ee622f68632354519981ae31e3f
Natalia Orlyk <oksenyuk.n@gmail.com>
2015-11-19 18:50:19 +0200

    chapter 2 task1

---
+++ src/main/java/book/chapter_2/HelloWorld.java
@@ -0,0 +1,11 @@
+package book.chapter_2;
+
+/**
+ * Created by home on 19.11.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Kosmonavtov 3a/52");
+        System.out.println("Hello World");
+    }
+}
