817b7228a6e0f99d3065c0d1a8851303048ad6d7
Natalia Orlyk <oksenyuk.n@gmail.com>
2015-11-20 15:00:24 +0200

    pet +petmaster  chapter 3

---
+++ src/main/java/book/chapter_3/Pet.java
@@ -0,0 +1,24 @@
+package book.chapter_3;
+
+/**
+ * Created by home on 20.11.2015.
+ */
+public class Pet {
+    int age;
+    float weight;
+    float height;
+    String color;
+
+    public void sleep() {
+        System.out.println("Спокойной ночи! До завтра!");
+    }
+    public void eat() {
+        System.out.println("Я очень голоден, давайте перекусим чипсами");
+    }
+    public String say(String aWord) {
+        String petResponse = "Ну ладно!" + aWord;
+        return petResponse;
+    }
+
+
+}
---
+++ src/main/java/book/chapter_3/PetMaster.java
@@ -0,0 +1,15 @@
+package book.chapter_3;
+
+/**
+ * Created by home on 20.11.2015.
+ */
+public class PetMaster {
+    public static void main(String[] args) {
+        String petReaction;
+        Pet myPet = new Pet();
+        myPet.eat();
+        petReaction = myPet.say("Чик!! Чирик!!");
+        System.out.println(petReaction);
+        myPet.sleep();
+    }
+}
