774be8aea4b224c9c80274c472ffd10589283534
Natalia Orlyk <oksenyuk.n@gmail.com>
2015-11-20 11:57:54 +0200

    Hello image chapter 2

--- src/main/java/book/chapter_2/HelloWorld.java
+++ src/main/java/book/chapter_2/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Kosmonavtov 3a/52");
         System.out.println("Hello World");
+        System.out.println("* * *** *   *   ***");
+        System.out.println("* * *   *   *   * *");
+        System.out.println("*** *** *   *   * *");
+        System.out.println("* * *   *   *   * *");
+        System.out.println("* * *** *** *** ***");
     }
 }
